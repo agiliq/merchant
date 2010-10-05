dadac02de8e8bf6550a9837091ad6d49ffa9be3e
ashok.raavi <ashok.raavi@gmail.com>
2010-10-05 19:06:44 +0530

    enable admin

--- example/settings.py
+++ example/settings.py
@@ -90,7 +90,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
 
     'app',
     'billing',
--- example/urls.py
+++ example/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 
     url(r'^', include('billing.urls')),
     url(r'^', include('app.urls')),
