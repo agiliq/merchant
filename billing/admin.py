78937711028fdfd63b925eeeb5d800f0c618b55f
ashok.raavi <ashok.raavi@gmail.com>
2010-10-06 12:35:51 +0530

    added admin.py

---
+++ billing/admin.py
@@ -0,0 +1,7 @@
+
+from django.contrib import admin
+
+import billing.models as billing_models
+
+admin.site.register(billing_models.GCNewOrderNotification)
+admin.site.register(billing_models.AuthorizeAIMResponse)
