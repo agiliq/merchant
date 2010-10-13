20e2c0c4dbb78ea3de614533329db623744c9fd9
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2010-10-13 11:37:42 +0530

    Code reorganization

--- billing/__init__.py
+++ billing/__init__.py
@@ -1,10 +1,1 @@
-"""
-Add lib directory to python path
-"""
-
-import os
-import sys
-from merchant import Merchant
-
-PATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(PATH, 'lib'))
+from gateway import Gateway
