7be455174bf42486ba1da4032d064f7e027dd314
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2010-10-13 14:08:33 +0530

    Fixing the gateway API

--- billing/gateway.py
+++ billing/gateway.py
@@ -1,22 +1,10 @@
 from django.utils.importlib import import_module
-import re
-from gateways import *
 
 class GatewayNotConfigured(Exception):
     pass
 
-class BaseGateway(object):
-    GATEWAYS = {
-        "PAYPAL": {"object": PaypalCardProcess, "transaction_field": "correlationid", "status": "ack"}
-        "AUTHORIZE_NET": {"object": AuthorizeNetGateway, "transaction_field": "transaction_id", "status": "response_reason_text"}
-        "EWAY": {"object": Eway, "transaction_field": "ewayTrxnNumber", "status": "ewayTrxnStatus"}
-        }
-
-    def __init__(self, gateway, *args, **kwargs):
-        gateway_filename = "%s_gateway" %gateway
-        gateway_modules = import_module("gateways.%s" %gateway_filename)
-        gateway_class_name = gateway_filename
-        self.gateway_obj = GATEWAYS[gateway]["object"](*args, **kwargs)
+class Gateway(object):
+    """Sub-classes to inherit from this and implement the below methods"""
 
     def purchase(self, money, credit_card, options = {}):
         raise NotImplementedError
@@ -42,8 +30,12 @@ class BaseGateway(object):
     def unstore(self, identification, options = {}):
         raise NotImplementedError
 
-    def charge(self, amount, credit_card, *args, **kwargs):
-        actual_response = self.gateway_obj.purchase(amount, credit_card, *args, **kwargs)
-        trnx_id = getattr(actual_response, self.gateway["transaction_field"], None)
-        status = getattr(actual_response, self.gateway["status"], None)
-        return {"transaction_id": trnx_id, "status": status, "actual": actual_response}
+def get_gateway(gateway, *args, **kwargs):
+    """Return the gateway instance"""
+    gateway_filename = "%s_gateway" %gateway
+    gateway_module = import_module("gateways.%s" %gateway_filename)
+    gateway_class_name = "".join(gateway_filename.title().split("_"))
+    try:
+        return getattr(gateway_module, gateway_class_name)(*args, **kwargs)
+    except AttributeError:
+        raise GatewayNotConfigured("Missing %s class in the gateway module." %gateway_class_name)
