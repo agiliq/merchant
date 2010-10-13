1465acd526f64ad57655e46c2ce40f2fac36aa4f
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2010-10-13 11:52:44 +0530

    Writing the methods for the base gateway class

--- billing/gateway.py
+++ billing/gateway.py
@@ -1,9 +1,11 @@
+from django.utils.importlib import import_module
+import re
 from gateways import *
 
 class GatewayNotConfigured(Exception):
     pass
 
-class Merchant(object):
+class BaseGateway(object):
     GATEWAYS = {
         "PAYPAL": {"object": PaypalCardProcess, "transaction_field": "correlationid", "status": "ack"}
         "AUTHORIZE_NET": {"object": AuthorizeNetGateway, "transaction_field": "transaction_id", "status": "response_reason_text"}
@@ -11,10 +13,34 @@ class Merchant(object):
         }
 
     def __init__(self, gateway, *args, **kwargs):
-        if gateway not in self.GATEWAYS:
-            raise GatewayNotConfigured("Missing gateway.")
-        self.gateway = GATEWAYS[gateway]
+        gateway_filename = "%s_gateway" %gateway
+        gateway_modules = import_module("gateways.%s" %gateway_filename)
+        gateway_class_name = gateway_filename
         self.gateway_obj = GATEWAYS[gateway]["object"](*args, **kwargs)
+
+    def purchase(self, money, credit_card, options = {}):
+        raise NotImplementedError
+
+    def authorize(self, money, credit_card, options = {}):
+        raise NotImplementedError
+
+    def capture(self, money, authorization, options = {}):
+        raise NotImplementedError
+
+    def void(self, identification, options = {}):
+        raise NotImplementedError
+
+    def credit(self, money, identification, options = {}):
+        raise NotImplementedError
+
+    def recurring(self, money, creditcard, options = {}):
+        raise NotImplementedError
+
+    def store(self, creditcard, options = {}):
+        raise NotImplementedError
+
+    def unstore(self, identification, options = {}):
+        raise NotImplementedError
 
     def charge(self, amount, credit_card, *args, **kwargs):
         actual_response = self.gateway_obj.purchase(amount, credit_card, *args, **kwargs)
