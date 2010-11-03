8567ed6af2962741b5659f8889d554bbb85dadf4
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2010-11-03 11:28:08 +0530

    Uncommenting other gateway tests so that all of them run.

--- billing/tests/__init__.py
+++ billing/tests/__init__.py
@@ -1,7 +1,7 @@
 import unittest
-# from gateway_tests import *
-# from authorize_net_tests import *
-# from pay_pal_tests import *
+from gateway_tests import *
+from authorize_net_tests import *
+from pay_pal_tests import *
 from eway_tests import *
 
 if __name__ == "__main__":
