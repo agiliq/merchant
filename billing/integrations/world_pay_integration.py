4c125626e6c83d2c32e024031702a1771fbc5a2f
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-03-09 15:11:19 +0530

    Removed the older templatetag and added a link to the WorldPay Developer docs.

--- billing/integrations/world_pay_integration.py
+++ billing/integrations/world_pay_integration.py
@@ -3,12 +3,15 @@ from billing.integration import Integration
 RBS_HOSTED_URL_TEST = "https://select-test.wp3.rbsworldpay.com/wcc/purchase"
 RBS_HOSTED_URL_LIVE = "https://secure.wp3.rbsworldpay.com/wcc/purchase"
 
+# http://www.rbsworldpay.com/support/bg/index.php?page=development&sub=integration&c=WW
+
 class WorldPayIntegration(Integration):
     # Template for required fields
     fields = {"instId": "",
               "cart_id": "",
               "amount": "",
-              "currency": ""}
+              "currency": "",
+              "testMode": 100}
 
     def get_urls(self):
         pass
