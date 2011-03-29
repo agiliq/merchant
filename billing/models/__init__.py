e7501a54cc6515613cabe1bc1d5a6ecf3f998045
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-03-29 14:33:06 +0530

    Added the AmazonFPS Response to the list of models.

--- billing/models/__init__.py
+++ billing/models/__init__.py
@@ -2,3 +2,4 @@ from authorize_models import AuthorizeAIMResponse
 from gc_models import GCNewOrderNotification
 from world_pay_models import WorldPayResponse
 from eway_models import EwayResponse
+from amazon_fps_models import AmazonFPSResponse
