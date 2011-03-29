cb953589267cfc7939a90618c58148ac7bb2017c
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-03-29 14:31:22 +0530

    Added the AmazonFPS Response model.

---
+++ billing/models/amazon_fps_models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class AmazonFPSResponse(models.Model):
+    buyerEmail = models.EmailField()
+    buyerName = models.CharField(max_length=75)
+    callerReference = models.CharField(max_length=100)
+    notificationType = models.CharField(max_length=50)
+    operation = models.CharField(max_length=20)
+    paymentMethod = models.CharField(max_length=5)
+    recipientEmail = models.EmailField()
+    recipientName = models.CharField(max_length=75)
+    statusCode = models.CharField(max_length=50)
+    statusMessage = models.TextField()
+    transactionAmount = models.FloatField()
+    transactionDate = models.DateTimeField()
+    transactionId = models.CharField(max_length=50)
+    transactionStatus = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return "%s : %s" %(self.transactionId, self.statusCode)
