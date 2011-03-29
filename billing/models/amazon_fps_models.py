c7aaca173f3c2f5097c0b74e90646c0082a59f4a
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-03-29 15:21:48 +0530

    Fixed a typo. index -> db_index.

--- billing/models/amazon_fps_models.py
+++ billing/models/amazon_fps_models.py
@@ -13,7 +13,7 @@ class AmazonFPSResponse(models.Model):
     statusMessage = models.TextField()
     transactionAmount = models.FloatField()
     transactionDate = models.DateTimeField()
-    transactionId = models.CharField(max_length=50, index=True)
+    transactionId = models.CharField(max_length=50, db_index=True)
     transactionStatus = models.CharField(max_length=50)
 
     def __unicode__(self):
