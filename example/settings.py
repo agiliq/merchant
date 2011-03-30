843b410e125107a9651a3bdf3579fa4863022012
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-03-30 11:59:10 +0530

    Added static files in the app to allow for static files of the admin to be served.

--- example/settings.py
+++ example/settings.py
@@ -89,7 +89,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
+    'django.contrib.staticfiles',
     'django.contrib.admin',
 
     'app',
