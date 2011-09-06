eb49f6f147e3cd04c4848cf55fc835c4f387031b
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-09-06 18:45:28 +0530

    Added the tests for the braintree payments server to server gateway (S2S).

---
+++ billing/tests/braintree_payments_tests.py
@@ -0,0 +1,72 @@
+from django.test import TestCase
+from billing import get_gateway, CreditCard
+from billing.signals import *
+from billing.models import BraintreePaymentResponse
+from billing.gateway import CardNotSupported
+from billing.utils.credit_card import Visa
+
+class AuthorizeNetAIMGatewayTestCase(TestCase):
+    def setUp(self):
+        self.merchant = get_gateway("braintree_payment")
+        self.merchant.test_mode = True
+        self.credit_card = CreditCard(first_name="Test", last_name="User",
+                                      month=10, year=2011, 
+                                      number="4111111111111111", 
+                                      verification_value="100")
+
+    def testCardSupported(self):
+        self.credit_card.number = "5019222222222222"
+        self.assertRaises(CardNotSupported, 
+                          lambda : self.merchant.purchase(1000, self.credit_card))
+
+    def testCardValidated(self):
+        self.merchant.test_mode = False
+        self.credit_card.number = "4005519200000004"
+        self.assertFalse(self.merchant.validate_card(self.credit_card))
+
+    def testCardType(self):
+        self.merchant.validate_card(self.credit_card)
+        self.assertEquals(self.credit_card.card_type, Visa)
+
+    def testPurchase(self):
+        resp = self.merchant.purchase(1, self.credit_card)
+        self.assertEquals(resp["status"], "SUCCESS")
+        self.assertTrue(isinstance(resp["response"], BraintreePaymentResponse)) 
+
+    def testFailedPurchase(self):
+        resp = self.merchant.purchase(2001, self.credit_card)
+        self.assertEquals(resp["status"], "FAILURE")
+
+    def testDeclinedPurchase(self):
+        resp = self.merchant.purchase(2900, self.credit_card)
+        self.assertEquals(resp["status"], "FAILURE")
+
+    def testPaymentSuccessfulSignal(self):
+        received_signals = []
+
+        def receive(sender, **kwargs):
+            received_signals.append(kwargs.get("signal"))
+
+        transaction_was_successful.connect(receive)
+
+        resp = self.merchant.purchase(1, self.credit_card)
+        self.assertEquals(received_signals, [transaction_was_successful])
+
+    def testPaymentUnSuccessfulSignal(self):
+        received_signals = []
+
+        def receive(sender, **kwargs):
+            received_signals.append(kwargs.get("signal"))
+
+        transaction_was_unsuccessful.connect(receive)
+
+        resp = self.merchant.purchase(2000, self.credit_card)
+        self.assertEquals(received_signals, [transaction_was_unsuccessful])
+
+    def testCreditCardExpired(self):
+        credit_card = CreditCard(first_name="Test", last_name="User",
+                                 month=10, year=2011, 
+                                 number="4000111111111115", 
+                                 verification_value="100")
+        resp = self.merchant.purchase(100, credit_card)
+        self.assertNotEquals(resp["status"], "SUCCESS")
