1fd54bf3d173d1d4a145a09421d97d8d62e5f6a1
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2011-09-07 19:03:03 +0530

    Added the transaction_was_successful and transaction_was_unsuccessful in the PayPal integration. Fixes #9.

--- billing/integrations/pay_pal_integration.py
+++ billing/integrations/pay_pal_integration.py
@@ -2,6 +2,8 @@ from billing import Integration
 from django.conf import settings
 from paypal.standard.conf import POSTBACK_ENDPOINT, SANDBOX_POSTBACK_ENDPOINT
 from django.conf.urls.defaults import patterns, include
+from paypal.standard.ipn.signals import payment_was_flagged, payment_was_successful
+from billing.signals import transaction_was_successful, transaction_was_unsuccessful
 
 class PayPalIntegration(Integration):
     def __init__(self):
@@ -26,3 +28,16 @@ class PayPalIntegration(Integration):
            (r'^', include('paypal.standard.ipn.urls')),
             )
         return urlpatterns
+
+def unsuccessful_txn_handler(sender, **kwargs):
+    transaction_was_unsuccesful.send(sender=sender.__class__,
+                                     type="purchase",
+                                     response=self)
+
+def successful_txn_handler(sender, **kwargs):
+    transaction_was_succesful.send(sender=sender.__class__,
+                                   type="purchase",
+                                   response=self)
+
+payment_was_flagged.connect(unsuccessful_txn_handler)
+payment_was_successful.connect(successful_txn_handler)
