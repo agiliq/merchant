bbbfc9df16c611e783f036f206214a46ef86f4f0
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2012-01-27 13:23:35 +0530

    First stab at Samurai.js.

--- billing/integrations/samurai_integration.py
+++ billing/integrations/samurai_integration.py
@@ -2,26 +2,25 @@ from billing import Integration
 from django.conf import settings
 from django.conf.urls.defaults import patterns, url
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
 from billing.forms.samurai_forms import SamuraiForm
 
 
 class SamuraiIntegration(Integration):
     def __init__(self):
         super(SamuraiIntegration, self).__init__()
+        self.merchant_key = settings.SAMURAI_MERCHANT_KEY
 
     def generate_form(self):
         initial_data = self.fields
         form = SamuraiForm(initial=initial_data)
         return form
-
         
     @csrf_exempt
-    def get_token(self, request):
-        token = request.POST['samuraiToken']
-        return HttpResponse('Success')
+    def transaction(self, request):
+        raise NotImplementedError
 
     def get_urls(self):
         urlpatterns = patterns('',
-           url('^samurai-get-token/$', self.get_token, name="get_token"),)
+           url('^samurai_token/$', self.transaction, name="samurai_transaction")
+        )
         return urlpatterns
