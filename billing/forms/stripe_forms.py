f3762aa6de14fe7478166024e3b62c585c3053c4
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2012-01-27 12:10:59 +0530

    Removed the additional underscore in the stripe forms.

--- billing/forms/stripe_forms.py
+++ billing/forms/stripe_forms.py
@@ -1,8 +1,7 @@
 from django import forms
 
-
 class StripeForm(forms.Form):
-    credit_card__number = forms.CharField()
-    credit_card__cvc = forms.CharField(max_length=4)
-    credit_card__expiration_month = forms.CharField(max_length=7)
-    credit_card__expiration_year = forms.CharField(max_length=7)
+    credit_card_number = forms.CharField(max_length=16)
+    credit_card_cvc = forms.CharField(max_length=4)
+    credit_card_expiration_month = forms.CharField(max_length=2)
+    credit_card_expiration_year = forms.CharField(max_length=2)
