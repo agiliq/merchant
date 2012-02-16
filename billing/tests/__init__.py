66b895fb356ffb04a512cc59ffae1b2f1217b24a
Celso Pinto <celso@leftfield.pt>
2012-02-16 17:58:15 +0000

    skip tests on modules that depend on things that can't be imported

--- billing/tests/__init__.py
+++ billing/tests/__init__.py
@@ -1,15 +1,37 @@
 import unittest
 from gateway_tests import *
 from authorize_net_tests import *
-from pay_pal_tests import *
+
+try:
+    import paypal
+    from pay_pal_tests import *
+except ImportError:
+    pass
+    
 from eway_tests import *
 from world_pay_tests import *
 from google_checkout_tests import *
 from amazon_fps_tests import *
-from braintree_payments_tests import *
-from braintree_payments_tr_tests import *
-from stripe_tests import *
-from samurai_tests import *
+
+try:
+    import braintree
+    from braintree_payments_tests import *
+    from braintree_payments_tr_tests import *
+except ImportError:
+    pass
+    
+try:
+    import stripe
+    from stripe_tests import *
+except ImportError:
+    pass
+    
+try:
+    import samurai
+    from samurai_tests import *
+except ImportError:
+    pass
+    
 from paylane_tests import *
 
 if __name__ == "__main__":
