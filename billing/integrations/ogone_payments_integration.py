d2af20241d64eb3ccc07d69575b2ddde414999c5
arjunc77 <arjunchitturi@gmail.com>
2012-04-30 20:18:32 +0530

    pass ogone settings as a Bunch to be compatible with module

--- billing/integrations/ogone_payments_integration.py
+++ billing/integrations/ogone_payments_integration.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 *-*
-from billing import Integration
+from billing import Integration, IntegrationNotConfigured
 from django.conf import settings
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
 from django_ogone.ogone import Ogone
 
+
 class OgonePaymentsIntegration(Integration):
+    display_name = "Ogone Payments Integration"
+
     def __init__(self, options=None):
         if not options:
             options = {}
         super(OgonePaymentsIntegration, self).__init__(options=options)
+        merchant_settings = getattr(settings, "MERCHANT_SETTINGS")
+        if not merchant_settings or not merchant_settings.get("ogone_payments"):
+            raise IntegrationNotConfigured("The '%s' integration is not correctly "
+                                       "configured." % self.display_name)
+        self.ogone_payments_settings = merchant_settings["ogone_payments"]
         self.ogone = Ogone
         self.data = {}
 
@@ -22,14 +27,10 @@ class OgonePaymentsIntegration(Integration):
         pass
 
     def ogone_success_handler(self, request, response=None):
-        return render_to_response("billing/ogone_success.html",
-                                  {"response": response},
-                                  context_instance=RequestContext(request))
+        pass
 
     def ogone_failure_handler(self, request, response=None):
-        return render_to_response("billing/ogone_failure.html",
-                                  {"response": response},
-                                  context_instance=RequestContext(request))
+        pass
 
     def get_urls(self):
         pass
@@ -39,29 +40,34 @@ class OgonePaymentsIntegration(Integration):
 
     def generate_tr_data(self):
         data = {
-            'orderID': 17,
+            'orderID': 21,
             'ownerstate': u'',
             'cn': u'Venkata Ramana',
             'language': 'en_US',
             'ownertown': u'Hyderabad',
             'ownercty': u'IN',
-            'exceptionurl': u'http://127.0.0.1:8000/failure/',
+            'exceptionurl': u'http://127.0.0.1:8000/offsite/ogone/failure/',
             'ownerzip': u'Postcode',
-            #'catalogurl': u'http://127.0.0.1:8000/',
+            'catalogurl': u'http://127.0.0.1:8000/',
             'currency': u'EUR',
             'amount': u'579',
-            'declineurl': u'http://127.0.0.1:8000/failure/',
+            'declineurl': u'http://127.0.0.1:8000/offsite/ogone/failure/',
             'homeurl': u'http://127.0.0.1:8000/',
-            'cancelurl': u'http://127.0.0.1:8000/failure/',
-            'accepturl': u'http://127.0.0.1:8000/success/',
+            'cancelurl': u'http://127.0.0.1:8000/offsite/ogone/failure/',
+            'accepturl': u'http://127.0.0.1:8000/offsite/ogone/success/',
             'owneraddress': u'Near Madapur PS',
-            'com': u'Order #14: Venkata Ramana',
+            'com': u'Order #21: Venkata Ramana',
             'email': u'ramana@agiliq.com'
             }
-        data['PSPID'] = settings.PSPID
         return data
 
     def generate_form(self):
+        class Bunch(dict):
+            def __init__(self, **kw):
+                dict.__init__(self, kw)
+                self.__dict__ = self
         self.data = self.generate_tr_data()
-        form = self.ogone.get_form(self.data, settings=settings)
+        bunch = Bunch()
+        bunch.update(self.ogone_payments_settings)
+        form = self.ogone.get_form(self.data, settings=bunch)
         return form
