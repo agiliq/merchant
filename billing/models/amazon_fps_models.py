bcdef9a18694dc9bf6aa7feb795d0980b996eb54
Ken Cochrane <KenCochrane@gmail.com>
2013-01-28 09:53:21 -0500

    Added some missing fields to the AmazonFPSResponse taken from http://docs.aws.amazon.com/AmazonFPS/latest/FPSAPIReference/AWSFPSIPNDetails.html. All new fields have been added as null and blank true, so they won't break any existing Apps. I didn't see any way to do migrations (i.e. South) so I'm not sure what the proceedure is for this. Another idea is to add a new Model and then link to them one to one, but that is a little more complicated.

--- billing/models/amazon_fps_models.py
+++ billing/models/amazon_fps_models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class AmazonFPSResponse(models.Model):
+    """ See This doc for a list of fields available, and what they mean.
+    http://docs.aws.amazon.com/AmazonFPS/latest/FPSAPIReference/AWSFPSIPNDetails.html """
     buyerEmail = models.EmailField()
     buyerName = models.CharField(max_length=75)
     callerReference = models.CharField(max_length=100)
@@ -17,6 +19,17 @@ class AmazonFPSResponse(models.Model):
     transactionDate = models.DateTimeField()
     transactionId = models.CharField(max_length=50, db_index=True)
     transactionStatus = models.CharField(max_length=50)
+
+    customerEmail = models.EmailField(blank=True, null=True)
+    customerName = models.CharField(max_length=75, blank=True, null=True)
+    # Address fields
+    addressFullName = models.CharField(max_length=100, blank=True, null=True)
+    addressLine1 = models.CharField(max_length=100, blank=True, null=True)
+    addressLine2 = models.CharField(max_length=100, blank=True, null=True)
+    addressState = models.CharField(max_length=50, blank=True, null=True)
+    addressZip = models.CharField(max_length=25, blank=True, null=True)
+    addressCountry = models.CharField(max_length=25, blank=True, null=True)
+    addressPhone = models.CharField(max_length=25, blank=True, null=True)
 
     def __unicode__(self):
         return "%s : %s" % (self.transactionId, self.statusCode)
