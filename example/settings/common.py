24b18ce8d5ea470ae16977427f1417ae4547c3aa
Javed Khan <javed@agiliq.com>
2013-04-22 14:35:47 +0530

    fix error with gunicorn

--- example/settings/common.py
+++ example/settings/common.py
@@ -76,7 +76,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'example.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
