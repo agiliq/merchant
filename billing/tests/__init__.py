21e629b69e47fc0c70e9aa5cd373083630eb2ba6
Venkata Ramana <ramana@agiliq.com>
2013-05-16 17:35:11 +0530

    remove merge conflicts

--- billing/tests/__init__.py
+++ billing/tests/__init__.py
@@ -12,13 +12,11 @@ from eway_tests import *
 from world_pay_tests import *
 from google_checkout_tests import *
 from amazon_fps_tests import *
-<<<<<<< HEAD
 from braintree_payments_tests import *
 from braintree_payments_tr_tests import *
 from stripe_tests import *
 from samurai_tests import *
 from ogone_payments_tests import *
-=======
 
 try:
     import braintree
@@ -42,7 +40,6 @@ except ImportError:
 from paylane_tests import *
 from chargebee_tests import *
 from bitcoin_tests import *
->>>>>>> 506c53506fbd6a0472caea205043db8217ab68f4
 
 if __name__ == "__main__":
     unittest.main()
