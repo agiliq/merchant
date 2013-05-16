4c9bf390f645a5bc566a1cf639049ff65beef567
Venkata Ramana <ramana@agiliq.com>
2013-05-16 19:08:45 +0530

    add ogone tests

--- billing/tests/__init__.py
+++ billing/tests/__init__.py
@@ -12,11 +12,6 @@ from eway_tests import *
 from world_pay_tests import *
 from google_checkout_tests import *
 from amazon_fps_tests import *
-from braintree_payments_tests import *
-from braintree_payments_tr_tests import *
-from stripe_tests import *
-from samurai_tests import *
-from ogone_payments_tests import *
 
 try:
     import braintree
@@ -40,6 +35,7 @@ except ImportError:
 from paylane_tests import *
 from chargebee_tests import *
 from bitcoin_tests import *
+from ogone_payments_tests import *
 
 if __name__ == "__main__":
     unittest.main()
--- billing/tests/ogone_payments_tests.py
+++ billing/tests/ogone_payments_tests.py
@@ -1,7 +1,5 @@
 from django.test import TestCase
-from django.utils.html import strip_spaces_between_tags
 from billing import get_integration
-from django.template import Template, Context
 
 
 class OgonePaymentsTestCase(TestCase):
@@ -26,10 +24,27 @@ class OgonePaymentsTestCase(TestCase):
             'owneraddress': u'Near Madapur PS',
             'com': u'Order #21: Venkata Ramana',
             'email': u'ramana@agiliq.com'
-            }
+        }
+        self.op.add_fields(self.data)
 
-    def testFormGen(self):
-        tmpl = Template("{% load ogone_payments from ogone_payments_tags %}{% ogone_payments obj %}")
-        form = tmpl.render(Context({"obj": self.op}))
-        pregen_form = """<form method="post" action="https://secure.ogone.com/ncol/test/orderstandard.asp"><input type="hidden" name="orderID" value="21" id="id_orderID" /><input type="hidden" name="ownerstate" id="id_ownerstate" /><input type="hidden" name="cn" value="Venkata Ramana" id="id_cn" /><input type="hidden" name="language" value="en_US" id="id_language" /><input type="hidden" name="PSPID" value="ramana" id="id_PSPID" /><input type="hidden" name="ownertown" value="Hyderabad" id="id_ownertown" /><input type="hidden" name="ownercty" value="IN" id="id_ownercty" /><input type="hidden" name="exceptionurl" value="http://127.0.0.1:8000/offsite/ogone/failure/" id="id_exceptionurl" /><input type="hidden" name="catalogurl" value="http://127.0.0.1:8000/" id="id_catalogurl" /><input type="hidden" name="email" value="ramana@agiliq.com" id="id_email" /><input type="hidden" name="currency" value="EUR" id="id_currency" /><input type="hidden" name="ownerzip" value="Postcode" id="id_ownerzip" /><input type="hidden" name="declineurl" value="http://127.0.0.1:8000/offsite/ogone/failure/" id="id_declineurl" /><input type="hidden" name="SHASign" value="B7AE34D419CCFDCDFB1CAE3E6F0D5C3E701FC2CE1C567EFB58636D775155038E002034FC9702FCAE2E0F726BB8BB787DC8E468446ABBFA6A1D19DDA9E40634C6" id="id_SHASign" /><input type="hidden" name="homeurl" value="http://127.0.0.1:8000/" id="id_homeurl" /><input type="hidden" name="cancelurl" value="http://127.0.0.1:8000/offsite/ogone/failure/" id="id_cancelurl" /><input type="hidden" name="amount" value="579" id="id_amount" /><input type="hidden" name="owneraddress" value="Near Madapur PS" id="id_owneraddress" /><input type="hidden" name="com" value="Order #21: Venkata Ramana" id="id_com" /><input type="hidden" name="accepturl" value="http://127.0.0.1:8000/offsite/ogone/success/" id="id_accepturl" /><input type="submit" value="checkout!"></form>"""
-        self.assertEquals(pregen_form, strip_spaces_between_tags(form).strip())
+    def testFormFields(self):
+        self.assertEquals(self.op.fields, {
+            'orderID': 21,
+            'ownerstate': u'',
+            'cn': u'Venkata Ramana',
+            'language': 'en_US',
+            'ownertown': u'Hyderabad',
+            'ownercty': u'IN',
+            'exceptionurl': u'http://127.0.0.1:8000/offsite/ogone/failure/',
+            'ownerzip': u'Postcode',
+            'catalogurl': u'http://127.0.0.1:8000/',
+            'currency': u'EUR',
+            'amount': u'579',
+            'declineurl': u'http://127.0.0.1:8000/offsite/ogone/failure/',
+            'homeurl': u'http://127.0.0.1:8000/',
+            'cancelurl': u'http://127.0.0.1:8000/offsite/ogone/failure/',
+            'accepturl': u'http://127.0.0.1:8000/offsite/ogone/success/',
+            'owneraddress': u'Near Madapur PS',
+            'com': u'Order #21: Venkata Ramana',
+            'email': u'ramana@agiliq.com'
+        })
