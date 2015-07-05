ba88ce0684995c22e7b4d49486ac2ff7f0aaf6eb
tyc85 <steven.tychen@gmail.com>
2015-07-04 18:51:42 -0700

    fix test_stripe.py

--- billing/tests/test_stripe.py
+++ billing/tests/test_stripe.py
@@ -41,8 +41,10 @@ class StripeGatewayTestCase(TestCase):
     def testStoreWithoutBillingAddress(self):
         resp = self.merchant.store(self.credit_card)
         self.assertEquals(resp["status"], "SUCCESS")
-        self.assertEquals(resp["response"].active_card.exp_month, self.credit_card.month)
-        self.assertEquals(resp["response"].active_card.exp_year, self.credit_card.year)
+        self.assertEquals(resp["response"]["sources"]["data"][0].exp_month,
+                          self.credit_card.month)
+        self.assertEquals(resp["response"]["sources"]["data"][0].exp_year,
+                          self.credit_card.year)
         self.assertTrue(getattr(resp["response"], "id"))
         self.assertTrue(getattr(resp["response"], "created"))
 
@@ -66,7 +68,7 @@ class StripeGatewayTestCase(TestCase):
         options = {"plan_id": plan_id}
         resp = self.merchant.recurring(self.credit_card, options=options)
         self.assertEquals(resp["status"], "SUCCESS")
-        subscription = resp["response"].subscription
+        subscription = resp["response"].subscriptions["data"][0]
         self.assertEquals(subscription.status, "active")
 
     def testCredit(self):
